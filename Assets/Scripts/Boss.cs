ec1b407679fc5544e4e0a08edb79c8f0de4e0176
Nick-C6013 <ncoop004@odu.edu>
2025-12-06 19:43:51 -0500

    Boss development

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [Header("Combat")]
+    public GameObject bulletPrefab;
+    public Transform[] Firepoint;
+    public float[] Attackspeed;
+    public float[] timer;
+    public int attackDamage = 1;
+
+
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        // NEW: Don't move when paused
+        if (GameManager.Instance != null && GameManager.Instance.IsPaused())
+        {
+            return;
+        }
+
+        if (timer[0] < Attackspeed[0])
+            timer[0] += Time.deltaTime;
+        if (timer[1] < Attackspeed[1])
+            timer[1] += Time.deltaTime;
+        if (timer[2] < Attackspeed[2])
+            timer[2] += Time.deltaTime;
+        Fire(0);
+        Fire(1);
+        Fire(2);
+
+    }
+    public void Fire(int index)
+    {
+        if (timer[index] >= Attackspeed[index])
+        {
+            if (bulletPrefab != null && Firepoint != null)
+            {
+                GameObject bullet = Instantiate(bulletPrefab, Firepoint[index].position, Quaternion.identity);
+                bullet.GetComponent<Enemybullet>().direction = Mathf.Sign(transform.localScale.x);
+                //AudioManager.Instance.PlayShootSound();
+                if (index == 1) 
+                {
+                    bullet.transform.localScale *= 8;
+                    bullet.GetComponent<CircleCollider2D>().radius *= 6;
+                    bullet.GetComponent<Enemybullet>().speed /= 2;
+                }
+            }
+            timer[index] = 0;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.TakeDamage(attackDamage);
+            }
+        }
+    }
+
+}     
+
---
+++ Assets/Scripts/BossHealthManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BossHealthManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
